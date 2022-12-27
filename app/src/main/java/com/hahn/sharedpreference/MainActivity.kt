234b7081547bc06eb2371e057e90831e606d5bae
andrehahn05 <andrehahn05@gmail.com>
2022-12-26 23:50:16 -0400

    snackbar

--- app/src/main/java/com/hahn/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/hahn/sharedpreference/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.hahn.sharedpreference
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import com.hahn.sharedpreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +13,9 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate((layoutInflater))
     }
     private var color = ""
-    companion object{
-        const val PREFERENCES_FILE ="PreferencesFile"
+
+    companion object {
+        const val PREFERENCES_FILE = "PreferencesFile"
     }
 
     override fun onCreate(savedInstanceState : Bundle?) {
@@ -38,12 +42,21 @@ class MainActivity : AppCompatActivity() {
         binding.layoutMain.setBackgroundColor(Color.parseColor(color))
 
         binding.btnSaveColor.setOnClickListener { view ->
-            val preference = getSharedPreferences(PREFERENCES_FILE, MODE_PRIVATE)
+            val preference = getSharedPreferences(PREFERENCES_FILE , MODE_PRIVATE)
             val editor = preference.edit()
-            editor.putString("color",color)
+            editor.putString("color" , color)
             editor.apply()
-
-
+            snackbar(view)
         }
+    }
+
+    private fun snackbar(view : View){
+        val snackbar = Snackbar.make(view,"Cor de fundo alterada com sucesso!",Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("OK"){ }
+        snackbar.setActionTextColor(Color.BLUE)
+        snackbar.setBackgroundTint(Color.WHITE)
+        snackbar.setTextColor(Color.BLACK)
+        snackbar.show()
+
     }
 }
