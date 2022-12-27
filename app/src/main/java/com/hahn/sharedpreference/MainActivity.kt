b56133e763004930f97785b7efb97ff8c5dea94b
andrehahn05 <andrehahn05@gmail.com>
2022-12-26 23:42:28 -0400

    sharedpreference

--- app/src/main/java/com/hahn/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/hahn/sharedpreference/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.hahn.sharedpreference
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hahn.sharedpreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate((layoutInflater))
+    }
+    private var color = ""
+    companion object{
+        const val PREFERENCES_FILE ="PreferencesFile"
+    }
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportActionBar !!.hide()
+
+        binding.color1.setOnClickListener {
+            color = "#FF03DAC5"
+        }
+        binding.color2.setOnClickListener {
+            color = "#FFBB86FC"
+        }
+        binding.color3.setOnClickListener {
+            color = "#03A9F4"
+        }
+        binding.color4.setOnClickListener {
+            color = "#FF0000"
+        }
+    }
+
+    private fun save(color : String) {
+        binding.layoutMain.setBackgroundColor(Color.parseColor(color))
+
+        binding.btnSaveColor.setOnClickListener { view ->
+            val preference = getSharedPreferences(PREFERENCES_FILE, MODE_PRIVATE)
+            val editor = preference.edit()
+            editor.putString("color",color)
+            editor.apply()
+
+
+        }
     }
 }
