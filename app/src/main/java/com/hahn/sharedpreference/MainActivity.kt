991eb7b30e6620b2832a215eb3267d74e1fe9a53
andrehahn05 <andrehahn05@gmail.com>
2022-12-27 19:03:52 -0400

    refactored

--- app/src/main/java/com/hahn/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/hahn/sharedpreference/MainActivity.kt
@@ -23,18 +23,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar !!.hide()
+        handlerOnclicklistener()
+    }
 
+    private fun handlerOnclicklistener() {
         binding.color1.setOnClickListener {
             color = "#FF03DAC5"
+            save(color)
         }
         binding.color2.setOnClickListener {
             color = "#FFBB86FC"
+            save(color)
         }
         binding.color3.setOnClickListener {
             color = "#03A9F4"
+            save(color)
         }
         binding.color4.setOnClickListener {
             color = "#FF0000"
+            save(color)
         }
     }
 
@@ -50,9 +57,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun snackbar(view : View){
-        val snackbar = Snackbar.make(view,"Cor de fundo alterada com sucesso!",Snackbar.LENGTH_INDEFINITE)
-        snackbar.setAction("OK"){ }
+    private fun snackbar(view : View) {
+        val snackbar = Snackbar.make(view ,
+            "Cor de fundo alterada com sucesso!" ,
+            Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("OK") { }
         snackbar.setActionTextColor(Color.BLUE)
         snackbar.setBackgroundTint(Color.WHITE)
         snackbar.setTextColor(Color.BLACK)
@@ -62,8 +71,8 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         val preference = getSharedPreferences(PREFERENCES_FILE , MODE_PRIVATE)
-        val color = preference.getString("cor","")
-        if(color!!.isNotEmpty()){
+        val color = preference.getString("cor" , "")
+        if (color !!.isNotEmpty()) {
             binding.layoutMain.setBackgroundColor(Color.parseColor(color))
         }
     }
